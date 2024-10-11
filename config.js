b50f16f8fbdb4eb4fcf12a69333deeb94c01607e
Luis Gómez <lufe023@gmail.com>
2024-10-11 19:55:46 -0400

    first Commit

--- config.js
+++ config.js
@@ -1,61 +1,22 @@
-//! const dotenv = require('dotenv')
-//! dotenv.config()
-
-require('dotenv').config()
+//? Dependencies
+require("dotenv").config();
 
 const config = {
-  port: process.env.PORT || 5000,
-  host: process.env.HOST || "http://localhost:5000",
-  nodeEnv: process.env.NODE_ENV || "dev", //? dev, prod, test
-  jwtSecret: process.env.JWT_SECRET,
-  db: {
-    dev: {
-      dialect: "postgres",
-      host: "localhost",
-      port: 5432,
-      database: "instagram-db",
-      username: "postgres",
-      password: "root",
-      //Extra configs
-      define: {
-        timestamps: true,
-        underscored: true,
-      },
+    port: process.env.PORT,
+    nodeEnv: process.env.NODE_ENV, //? Desarrollo, Testing, Produccion
+    jwtSecret: process.env.JWT_SECRET,
+    host: process.env.HOST,
+    frontendHost: process.env.FRONTENDHOST,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    db: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASS,
+        dbName: process.env.DB_NAME,
     },
-    prod: {
-      dialect: "postgres",
-      host: process.env.DB_PROD_HOST,
-      port: process.env.DB_PROD_PORT,
-      database: process.env.DB_PROD_NAME,
-      username: process.env.DB_PROD_USER,
-      password: process.env.DB_PROD_PASS,
-      //Extra configs
-      define: {
-        timestamps: true,
-        underscored: true,
-      },
-      //Esta configuracion es para produccion
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-    },
-    test: {
-      dialect: "postgres",
-      host: "localhost",
-      port: 5432,
-      database: "instagram-db",
-      username: "postgres",
-      password: "root",
-      //Extra configs
-      define: {
-        timestamps: true,
-        underscored: true,
-      },
-    },
-  },
 };
 
-module.exports = config
+module.exports = config;
+/**/
--- src/utils/database.js
+++ src/utils/database.js
@@ -1,9 +1,13 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require("sequelize");
+const config = require("../../config");
 
-const config = require('../../config')
+const db = new Sequelize({
+    dialect: "postgres",
+    host: config.db.host, //? Variable de entorno del host
+    port: config.db.port,
+    username: config.db.username, //? Variable de entorno del usuario
+    password: config.db.password, //? Variable de entorno de la contraseña
+    database: config.db.dbName, //? Variable de entorno de la base de datos
+});
 
-const environment = config.nodeEnv
-
-const db = new Sequelize(config.db[environment])
-
-module.exports = db
+module.exports = db;
